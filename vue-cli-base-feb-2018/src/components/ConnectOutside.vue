4cff45017e1d27ed17f6187d7fc7520e5f038db8
mikeyao <yaojunmike@gmail.com>
2019-02-20 16:07:06 -0800

    Added docker compose for client and express server

---
+++ vue-cli-base-feb-2018/src/components/ConnectOutside.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Call External API</h1>
+        <button @click='handler'>Get Message from outside!</button>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+const axios = require('axios')
+
+export default {
+    data (){
+        return {
+            message: '',
+        }
+    },
+    methods: {
+        handler (){
+            axios.get('http://localhost:3001/')
+                .then((response) => {
+                    console.log(response.data)
+                    this.message = response.data
+                })
+                .catch((error) => {
+                    this.message = 'Error occured.'
+                })
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
